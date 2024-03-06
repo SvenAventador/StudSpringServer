3c0ddec24e84cf482080c38dfd78a04fa2e5438d
SvenAventador <asumilkin67@gmail.com>
2024-03-06 23:29:04 +0300

    ...

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,8 +2,9 @@ const Router = require('express')
 const router = new Router()
 const userRoutes = require('./userRoutes')
 const personalRoutes = require('./personalRoutes')
+const instituteRoutes = require('./institutesRoutes')
 
 router.use('/user', userRoutes)
 router.use('/personal', personalRoutes)
-
+router.use('/institutes', instituteRoutes)
 module.exports = router
